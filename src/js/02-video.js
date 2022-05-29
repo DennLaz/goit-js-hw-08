f7fe6d7b79bc570395983c7b9b97f320dc4e62a1
Denis Lazarenko <dentus25@gmail.com>
2022-05-29 16:43:10 +0300

    add 02-vidoe.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+'use strict';
 
+import throttle from 'lodash.throttle';
+
+const VIDEOPLAYER_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  const currentTime = e.seconds;
+  localStorage.setItem(VIDEOPLAYER_TIME_KEY, currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_TIME_KEY));
