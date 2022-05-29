074edeeb999704575b5d7ae07dee5c50c35d1fe0
Denis Lazarenko <dentus25@gmail.com>
2022-05-29 17:26:12 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,7 +12,23 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(e) {
   const currentTime = e.seconds;
+
   localStorage.setItem(VIDEOPLAYER_TIME_KEY, currentTime);
 }
 
-player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_TIME_KEY));
+player
+  .setCurrentTime(localStorage.getItem(VIDEOPLAYER_TIME_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
